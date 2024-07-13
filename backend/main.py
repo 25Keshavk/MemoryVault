ffa57a20bb8a7a8a628d7dd74da14c93166c5f52
25Keshavk <25keshavk@students.harker.org>
2024-07-13 15:57:02 -0700

    get working setup for adding new content and querying???

--- backend/main.py
+++ backend/main.py
@@ -14,30 +14,37 @@ from langchain.chains import RetrievalQA
 
 load_dotenv()  # take environment variables from .env.
 
+from langchain.docstore.document import Document
 
-def add_document_to_pinecone():
+
+def get_vectorstore():
     index_name = "memory-vault"
     embeddings = OpenAIEmbeddings(
         model="text-embedding-3-small", openai_api_key=os.environ["OPENAI_API_KEY"]
     )
-
-    loader = TextLoader(os.path.join("texts", "sample.txt"))
-    documents = loader.load()
-
-    # do chunking
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    chunks = text_splitter.split_documents(documents)
-
-    # generate unique id for this doc that's shared across the chunks
-    document_id = str(uuid.uuid4())
-    for doc in chunks:
-        doc.metadata["document_id"] = document_id
-
     vectorstore = PineconeVectorStore(
         index_name=index_name,
         embedding=embeddings,
         pinecone_api_key=os.environ["PINECONE_API_KEY"],
     )
+
+    return vectorstore
+
+
+def add_document_to_pinecone(text: str):
+    new_doc = Document(page_content=text)
+
+    # do chunking for this new doc
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    chunks = text_splitter.split_documents([new_doc])
+
+    # generate unique id for this doc that's shared across the chunks
+    document_id = str(uuid.uuid4())
+    for chunk in chunks:
+        chunk.metadata["document_id"] = document_id
+
+    # get the vectorstore
+    vectorstore = get_vectorstore()
 
     vectorstore.add_documents(chunks)
 
@@ -46,10 +53,7 @@ def add_document_to_pinecone():
 # this function uses an llm to query the index about memories
 def get_llm_response(query: str):
     # get the vectorstore
-    index_name = "memory-vault"
-    vectorstore = PineconeVectorStore(
-        index_name=index_name, pinecone_api_key=os.environ["PINECONE_API_KEY"]
-    )
+    vectorstore = get_vectorstore()
 
     # completion llm
     llm = ChatOpenAI(
@@ -66,14 +70,13 @@ def get_llm_response(query: str):
 
 
 if __name__ == "__main__":
-    index_name = "memory-vault"
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-3-small", openai_api_key=os.environ["OPENAI_API_KEY"]
-    )
-    vectorstore = PineconeVectorStore(
-        index_name=index_name,
-        embedding=embeddings,
-        pinecone_api_key=os.environ["PINECONE_API_KEY"],
-    )
+    text = """
+                my name is keshav kotamraju. i like to watch the stars.
+            """
 
-    print(vectorstore)
+    add_document_to_pinecone(text)
+
+    query = "tell me about keshav"
+
+    res = get_llm_response(query)
+    print(res)
