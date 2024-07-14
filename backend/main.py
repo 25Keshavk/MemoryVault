45f6e5142290d169000d2ffe36092ce9def9683f
25Keshavk <25keshavk@students.harker.org>
2024-07-13 17:01:57 -0700

    Revert "temp"
    This reverts commit 003607f95cbd4f1b0401e0f293b750fb2b6f1f2a.

--- backend/main.py
+++ backend/main.py
@@ -16,68 +16,15 @@ load_dotenv()  # take environment variables from .env.
 
 from langchain.docstore.document import Document
 
-from urllib3 import make_headers
-from urllib.parse import urlparse
-from pinecone import Pinecone
-
-from langchain.document_loaders import TextLoader
-
-
-"""
-From the pinecone source code
-"""
-
-
-def get_pinecone_index(client: Pinecone, index_name: str):
-    indexes = client.list_indexes()
-    index_names = [i.name for i in indexes.index_list["indexes"]]
-
-    if index_name in index_names:
-        index = client.Index(index_name)
-    elif len(index_names) == 0:
-        raise ValueError(
-            "No active indexes found in your Pinecone project, "
-            "are you sure you're using the right Pinecone API key and Environment? "
-            "Please double check your Pinecone dashboard."
-        )
-    else:
-        raise ValueError(
-            f"Index '{index_name}' not found in your Pinecone project. "
-            f"Did you mean one of the following indexes: {', '.join(index_names)}"
-        )
-    return index
-
 
 def get_vectorstore():
-    # Create VectorDB client
+    index_name = "memory-vault"
     embeddings = OpenAIEmbeddings(
         model="text-embedding-3-small", openai_api_key=os.environ["OPENAI_API_KEY"]
     )
-    proxy_headers = None
-    proxy_uri = os.getenv("PROXY")  # http://username:password@myproxy.com
-
-    # If we have a proxy URI, set Pinecone to use it
-    """
-    Proxy code from https://github.com/langchain-ai/langchain/discussions/18763#discussioncomment-9446396
-    """
-    if proxy_uri:
-        url = urlparse(proxy_uri)
-        if url.username and url.password:
-            proxy_headers = make_headers(
-                proxy_basic_auth="%s:%s" % (url.username, url.password)
-            )
-    pinecone = Pinecone(
-        api_key=os.getenv("PINECONE_API_KEY"),
-        proxy_url=proxy_uri,
-        proxy_headers=proxy_headers,
-    )
-
-    index_name = "memory-vault"
-    index = get_pinecone_index(pinecone, index_name)
-
     vectorstore = PineconeVectorStore(
-        index,
-        embeddings,
+        index_name=index_name,
+        embedding=embeddings,
         pinecone_api_key=os.environ["PINECONE_API_KEY"],
     )
 
@@ -118,7 +65,28 @@ def get_llm_response(query: str):
         llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
     )
 
-    res = qa.invoke(query)
+    res = qa.run(query)
+    return res
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    text = """
+                my name is keshav kotamraju. i like to watch the stars.
+            """
+
+    # add_document_to_pinecone(text)
+
+    query = "what personality do you think keshav has"
+
+    res = get_llm_response(query)
+    print(res)
+
     return res
 
 
@@ -127,9 +95,9 @@ if __name__ == "__main__":
                 my name is keshav kotamraju. i like to watch the stars.
             """
 
-    add_document_to_pinecone(text)
+    # add_document_to_pinecone(text)
 
-    query = "tell me about keshav"
+    query = "what personality do you think keshav has"
 
     res = get_llm_response(query)
     print(res)
